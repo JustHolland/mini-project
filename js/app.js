fbf5918b11d9aea8c8c1745439cda52e1c8db693
justineholland <justineholland2@gmail.com>
2021-12-07 12:52:20 -0600

    phase2

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ class Dino {
   }
 }
 
-
+////////game commands////////
 
 alert("Wowzer! A tomagotchi egg is hatching" );
 
@@ -20,9 +20,13 @@ let namePrompt= prompt(' A Dinosaur!', 'Name your Dinosaur');
 const newDino = new Dino(namePrompt,10,10,10)
 
 const sleepbtn = document.querySelector("#sleepbtn")
-sleepbtn.addEventListener("click", off)
+sleepbtn.addEventListener("click", off);
+
 const body = document.querySelector("body")
-body.addEventListener("dblclick", on)
+body.addEventListener("dblclick", on);
+
+document.getElementById("feedbtn").addEventListener("click", feed);
+document.getElementById("exercisebtn").addEventListener("click", exercise);
 
 function off() {
   console.log("off clicked")
@@ -38,9 +42,15 @@ function on() {
   //while loop function on is true increase sleep points xx per xx seconds
 }
 
-// function feed() {
+function feed() {
+   console.log("feed clicked");
+   document.getElementById("feeding").style.display = "feeding"
+ }
 //   document.getElementById("feeding")style.display ="";
 // if loop if button clicked increase food by xx
 
-//function exercise()
+function exercise() {
+   console.log("exercise clicked");
+   document.getElementById("exercising").style.display= "block";
+ }
 // get getElementById("exercising")style.display = "";
