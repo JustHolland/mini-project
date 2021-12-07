7698f0e8f87d4386b34ed1494a2b56e2b26d6725
justineholland <justineholland2@gmail.com>
2021-12-07 10:33:55 -0600

    corrected  css light

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
-alert("Wowzer! A tamagotchi egg is hatching" );
+class Dino {
+  constructor(name,hunger,sleep, exercise){
+  this.name= name
+  this.hunger=hunger
+  this.sleep = sleep
+  this.exercise = exercise
+  }
+
+  sayHello(){
+    console.log("Hello! I am a dino!")
+  }
+}
+
+
+
+alert("Wowzer! A tomagotchi egg is hatching" );
 
 let namePrompt= prompt(' A Dinosaur!', 'Name your Dinosaur');
 
-class Dino {
-  constructor(name,hunger,sleep, exercise){
-  this.name= namePrompt
-  this.hunger=hunger
-  this.sleep
-  this.exercise = exercice
-  }
-}
-  function on() {
+const newDino = new Dino(namePrompt,10,10,10)
+
+const sleepbtn = document.querySelector("#sleepbtn")
+sleepbtn.addEventListener("click", off)
+const body = document.querySelector("body")
+body.addEventListener("dblclick", on)
+
+function off() {
+  console.log("off clicked")
   document.getElementById("overlay").style.display = "block";
-  alert("Dino is going to sleep,  click anywhere to turn the lights back on")
+  alert("Dino is going to sleep,  double click anywhere on the screen to turn the lights back on")
   //while loop function on is true increase sleep points xx per xx seconds
-  }
+}
 
 
-  function off() {
+function on() {
+  console.log("on clicked");
   document.getElementById("overlay").style.display = "none";
   //while loop function on is true increase sleep points xx per xx seconds
-  }
+}
 
 // function feed() {
 //   document.getElementById("feeding")style.display ="";
