f84ddba7405e6a57dafb609564dd40b7646b011a
justineholland <justineholland2@gmail.com>
2021-12-07 01:11:12 -0600

    dino skin added

--- js/app.js
+++ js/app.js
@@ -2,13 +2,29 @@ alert("Wowzer! A tamagotchi egg is hatching" );
 
 let namePrompt= prompt(' A Dinosaur!', 'Name your Dinosaur');
 
-function on() {
+class Dino {
+  constructor(name,hunger,sleep, exercise){
+  this.name= namePrompt
+  this.hunger=hunger
+  this.sleep
+  this.exercise = exercice
+  }
+}
+  function on() {
   document.getElementById("overlay").style.display = "block";
   alert("Dino is going to sleep,  click anywhere to turn the lights back on")
-  
-  //bonus add some time off animation to show dino sleeping
-}
+  //while loop function on is true increase sleep points xx per xx seconds
+  }
 
-function off() {
+
+  function off() {
   document.getElementById("overlay").style.display = "none";
-}
+  //while loop function on is true increase sleep points xx per xx seconds
+  }
+
+// function feed() {
+//   document.getElementById("feeding")style.display ="";
+// if loop if button clicked increase food by xx
+
+//function exercise()
+// get getElementById("exercising")style.display = "";
