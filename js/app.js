94a19fcb3006a19a8e11ab2fecefbd819ed4b747
justineholland <justineholland2@gmail.com>
2021-12-07 00:05:06 -0600

    dino sleep lights off works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-alert(Wowzer! tamagotchi  )
+alert("Wowzer! A tamagotchi egg is hatching" );
+
+let namePrompt= prompt(' A Dinosaur!', 'Name your Dinosaur');
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+  alert("Dino is going to sleep,  click anywhere to turn the lights back on")
+  
+  //bonus add some time off animation to show dino sleeping
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
